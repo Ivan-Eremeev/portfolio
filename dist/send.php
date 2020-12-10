3b1480bd4258c6fc55ffe6693dddb4b2426952ce
Ivan <ivan.frontcoder@gmail.com>
2020-12-10 23:17:18 +0500

    edit

--- dist/send.php
+++ dist/send.php
@@ -1,19 +1,19 @@
 <?php
-if ((isset($_POST['name']) && $_POST['tel'] != "")) { //Проверка отправилось ли наше поля name и не пустые ли они
+if ((isset($_POST['email']) && isset($_POST['message']) != "")) { //Проверка отправилось ли наше поля name и не пустые ли они
   $to = 'ivan.eremeev@yandex.ru'; //Почта получателя, через запятую можно указать сколько угодно адресов
-  $subject = ''.$_POST['subject'].'';
+  $subject = 'Письмо с портфолио';
   $message = '
   <html>
     <head>
       <title>' . $subject . '</title>
     </head>
     <body>
-      <p>Имя: ' . $_POST['name'] . '</p>
-      <p>Телефон: ' . $_POST['tel'] . '</p>
+      <p>Почта: ' . $_POST['email'] . '</p>
+      <p>Сообщение: ' . $_POST['message'] . '</p>
     </body>
   </html>';
   $headers = "Content-type: text/html; charset=utf-8 \r\n"; //Кодировка письма
-  $headers .= "From: Отправитель <".$to.">\r\n"; //Наименование и почта отправителя
+  $headers .= "From: Portfolio <".$to.">\r\n"; //Наименование и почта отправителя
   if (mail($to, $subject, $message, $headers)) {
     echo 'success';
   } else {
--- js-app/scripts.js
+++ js-app/scripts.js
@@ -172,93 +172,133 @@ $(document).ready(function () {
 	}
 
 	// Простая проверка форм на заполненность и отправка аяксом
-	// function formSubmit() {
-	$("[type=submit]").on('click', function (e){ 
-		e.preventDefault();
-		// Заводим переменные
-		// Ищем родительскую фору для того чтобы манипулировать элементами находящимися только внутри неё
-		var form = $(this).closest('.form');
-		// Запоминаем путь к php обработчику формы
-		var url = form.attr('action');
-		// Собираем все данные с полей формы для отправки
-		var form_data = form.serialize();
-		// Выбираем все обязательные поля по атрибуту required
-		var field = form.find('[required]');
+	function formSubmit() {
+		$("[type=submit]").on('click', function (e){ 
+			e.preventDefault();
+			// Заводим переменные
+			// Ищем родительскую фору для того чтобы манипулировать элементами находящимися только внутри неё
+			var form = $(this).closest('.form');
+			// Запоминаем путь к php обработчику формы
+			var url = form.attr('action');
+			// Собираем все данные с полей формы для отправки
+			var form_data = form.serialize();
+			// Выбираем все обязательные поля по атрибуту required
+			var field = form.find('[required]');
 
-		// Задаем количество пустых полей по умолчанию
-		empty = 0;
+			// Задаем количество пустых полей по умолчанию
+			empty = 0;
 
-		// Перебираем каждое обязательное поле
-		field.each(function() {
-			// Если поля пустые
-			if ($(this).val() == "") {
-				// Добавляем класс invalid
-				$(this).addClass('invalid');
-				// Увеличиваем счеткик пустых полей
-				empty++;
-			// Если поля не пустые
-			} else {
-				// Убираем класс invalid
-				$(this).removeClass('invalid');
-				// Добавляем класс valid если необходимо для стилизации
-				$(this).addClass('valid');
-			}  
-		});
-
-		// Можно проверить пересчет пустых полей в консоли
-		// console.log(empty);
-
-		// Если пустых полей больше 0
-		if (empty > 0) {
-			// Останавливаем работу скрипта запрещая отправку формы
-			return false;
-		// Если пустых полей нет
-		} else {        
-			// Запускаем отправку формы без перезагрузки страницы
-			$.ajax({
-				// Используем переменные в параметрах для отправки формы
-				url: url,
-				type: "POST",
-				dataType: "html",
-				data: form_data,
-				// При успешной отправке
-				// В аргумент response(произвольное название) можно записать и видеть результат ответа сервера
-				success: function (response) {
-					console.log(response);
-					// Дальше несколько вариантов
-					// Открываем окно с сообщением
-					// modalShow($('#success'));
-					// Открываем какую то страницу. как правило так называемую "страницу спасибо"
-					// document.location.href = "success.html";
-				},
-				// При ошибке отправки
-				error: function (response) {
-					console.log(response);
-					// Тоже что нибудь делаем
-					// modalShow($('#error'));
-					// Выводим в заготовленный блок какое то сообщение
-					// $('#rezult').text('Проверте корректность заполнения полей формы.');
-				}
+			// Перебираем каждое обязательное поле
+			field.each(function() {
+				// Если поля пустые
+				if ($(this).val() == "") {
+					// Добавляем класс invalid
+					$(this).addClass('invalid');
+					// Увеличиваем счеткик пустых полей
+					empty++;
+				// Если поля не пустые
+				} else {
+					// Убираем класс invalid
+					$(this).removeClass('invalid');
+					// Добавляем класс valid если необходимо для стилизации
+					$(this).addClass('valid');
+				}  
 			});
-		}
-	});
-	// Убираем класс invalid при снятии фокуса если поле не пустое
-	$('[required]').on('blur', function() {
-		if ($(this).val() != '') {
-			$(this).removeClass('invalid');
-		}
-	});
-	// Если есть чекбокс с политикой можно отключать кнопку при снятом чекбоксе добавляя к кнопке атрибут disabled 
-	$('.form__privacy input').on('change', function(event) {
-		event.preventDefault();
-		var btn = $(this).closest('.form').find('.btn');
-		if ($(this).prop('checked')) {
-			btn.removeAttr('disabled');
-			// console.log('checked');
-		} else {
-			btn.attr('disabled', true);
-		}
-	});
+
+			// Можно проверить пересчет пустых полей в консоли
+			// console.log(empty);
+
+			// Если пустых полей больше 0
+			if (empty > 0) {
+				// Останавливаем работу скрипта запрещая отправку формы
+				return false;
+			// Если пустых полей нет
+			} else {        
+				// Запускаем отправку формы без перезагрузки страницы
+				$.ajax({
+					// Используем переменные в параметрах для отправки формы
+					url: url,
+					type: "POST",
+					dataType: "html",
+					data: form_data,
+					// При успешной отправке
+					// В аргумент response(произвольное название) можно записать и видеть результат ответа сервера
+					success: function (response) {
+						// console.log(response);
+						// Дальше несколько вариантов
+						// Открываем окно с сообщением
+						modalShow($('#success'));
+						// Открываем какую то страницу. как правило так называемую "страницу спасибо"
+						// document.location.href = "success.html";
+					},
+					// При ошибке отправки
+					error: function (response) {
+						// console.log(response);
+						// Тоже что нибудь делаем
+						modalShow($('#error'));
+						// Выводим в заготовленный блок какое то сообщение
+						// $('#rezult').text('Проверте корректность заполнения полей формы.');
+					}
+				});
+			}
+		});
+		// Убираем класс invalid valid при снятии фокуса если поле не пустое
+		$('[required]').on('blur', function() {
+			if ($(this).val() != '') {
+				$(this).removeClass('invalid').removeClass('valid');
+			}
+		});
+		// Если есть чекбокс с политикой можно отключать кнопку при снятом чекбоксе добавляя к кнопке атрибут disabled 
+		// $('.form__privacy input').on('change', function(event) {
+		// 	event.preventDefault();
+		// 	var btn = $(this).closest('.form').find('.btn');
+		// 	if ($(this).prop('checked')) {
+		// 		btn.removeAttr('disabled');
+		// 		// console.log('checked');
+		// 	} else {
+		// 		btn.attr('disabled', true);
+		// 	}
+		// });
+	}
+	formSubmit();
+
+	// Модальное окно
+	function modal(modal) {
+		$('.modal-trigger').on('click', function() {
+			var $this = $(this),
+					data = $this.data('modal'),
+					thisModal = $(data);
+			modalShow(thisModal);
+		});
+	};
+	// Открытие модального окна
+	function modalShow(thisModal) {
+		var html = $('html'),
+				modalClose = thisModal.find($('.modal_close')),
+				documentWidth = parseInt(document.documentElement.clientWidth),
+				windowsWidth = parseInt(window.innerWidth),
+				scrollbarWidth = windowsWidth - documentWidth;
+		thisModal.show(0, function() {
+			setTimeout(thisModal.addClass('open'),500);
+		});
+		// html.addClass('lock').css('padding-right',scrollbarWidth);
+		modalClose.on('click', function() {
+			modalHide(thisModal);
+		});
+		thisModal.on('click', function(e) {
+			if (thisModal.has(e.target).length === 0) {
+				modalHide(thisModal);
+			}
+		});
+	};
+	// Закрытие модального окна
+	function modalHide(thisModal) {
+		var html = $('html');
+		thisModal.removeClass('open');
+		thisModal.hide();
+		// html.removeClass('lock').css('padding-right',0);
+	};
+	modal();
 
 	// Ленивая загрузка
 	$('img').lazy();
